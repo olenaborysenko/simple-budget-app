dbe14ee327610d1938aa4e4c9e77b2e423253791
Olena <31489876+olenaborysenko@users.noreply.github.com>
2018-04-09 14:43:10 +0300

    Created income and expense function constructors

--- app.js
+++ app.js
@@ -1,7 +1,57 @@
 // BUDGET CONTROLLER
 var budgetController = (function() {
 
-	//
+	var Expense = function(id, description, value) {
+		this.id = id;
+		this.description = description;
+		this.value = value;
+	};
+
+	var Income = function(id, description, value) {
+		this.id = id;
+		this.description = description;
+		this.value = value;
+	};
+
+	var data = {
+		allItems: {
+			exp: [],
+			inc: []
+		},
+		totals: {
+			exp: 0,
+			inc: 0
+		}
+	};
+
+	return {
+		addItem: function(type, des, val) {
+			var newItem, ID;
+
+			// create a new id by incrementing the last item's id
+			if (data.allItems[type].length > 0) {
+				ID = data.allItems[type][data.allItems[type].length - 1].id + 1; 
+			} else {
+				ID = 0;
+			}
+
+			// create a new item based on the 'inc' or 'exp' type
+			if (type === 'exp'){
+				newItem = new Expense(ID, des, val);
+			} else {
+				newItem = new Income(ID, des, val);
+			}
+
+			// push the new item into the data structure
+			data.allItems[type].push(newItem);
+			// return the new item
+			return newItem;
+		},
+
+		testing: function() {
+			console.log(data);
+		}
+	};
 
 })();
 
@@ -51,11 +101,13 @@ var controller = (function(budgetCtrl, UICtrl) {
 	};
 
 	var ctrlAddItem = function() {
+		var input, newItem;
+
 		// 1. Get the field input data
-		var input = UICtrl.getInput();
-		console.log(input);
+		input = UICtrl.getInput();
 
 		// 2. Add the item to the budget controller
+		newItem = budgetCtrl.addItem(input.type, input.description, input.value);
 
 		//3. Add the item to the UI
 
