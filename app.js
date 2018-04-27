cb569275dff8ce91c436185f958fa2d1efe866d4
Olena <31489876+olenaborysenko@users.noreply.github.com>
2018-04-27 19:58:57 +0300

    Added prevention of the false inputs

--- app.js
+++ app.js
@@ -73,7 +73,7 @@ var UIController = (function() {
 			return {
 				type: document.querySelector(DOMstrings.inputType).value, // 'inc' or 'exp'
 				description: document.querySelector(DOMstrings.inputDescription).value,
-				value: document.querySelector(DOMstrings.inputValue).value
+				value: parseFloat(document.querySelector(DOMstrings.inputValue).value)
 			};
 		},
 
@@ -137,25 +137,35 @@ var controller = (function(budgetCtrl, UICtrl) {
 		});
 	};
 
+	var updateBudget = function() {
+		//1. Calculate the budget
+
+		//2. Return the Budget
+
+		//3. Display the budget on the UI
+
+	}
+
 	var ctrlAddItem = function() {
 		var input, newItem;
 
 		// 1. Get the field input data
 		input = UICtrl.getInput();
 
-		// 2. Add the item to the budget controller
-		newItem = budgetCtrl.addItem(input.type, input.description, input.value);
+		if (input.description !== "" && !isNaN(input.value) && input.value > 0) {
+			// 2. Add the item to the budget controller
+			newItem = budgetCtrl.addItem(input.type, input.description, input.value);
 
-		//3. Add the item to the UI
-		UICtrl.addListItem(newItem, input.type);
+			//3. Add the item to the UI
+			UICtrl.addListItem(newItem, input.type);
 
-		//4. Clear the fields
-		UICtrl.clearFields();
+			//4. Clear the fields
+			UICtrl.clearFields();
 
-		//5. Calculate the budget
-
-		//6. Display the budget on the UI
-
+			// 5. Calculate and update budget
+			updateBudget();
+		}
+	
 	};
 
 	return {
