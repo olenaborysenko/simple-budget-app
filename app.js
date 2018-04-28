4dbb71be54b7297b3cf51ac92cfb3938c99ea879
Olena <31489876+olenaborysenko@users.noreply.github.com>
2018-04-28 16:55:31 +0300

    Added current date displaying

--- app.js
+++ app.js
@@ -146,7 +146,8 @@ var UIController = (function() {
 		expensesLabel: '.budget__expenses--value',
 		percentageLabel: '.budget__expenses--percentage',
 		container: '.container',
-		expensesPercLabel: '.item__percentage'
+		expensesPercLabel: '.item__percentage',
+		dateLabel: '.budget__title--month'
 	};
 
 	var formatNumber = function(number, type) {
@@ -254,6 +255,18 @@ var UIController = (function() {
 			});
 		},
 
+		displayMonth: function() {
+			var now, months, month, year;
+
+			now = new Date();
+
+			months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+			month = now.getMonth();
+			year = now.getFullYear();
+
+			document.querySelector(DOMstrings.dateLabel).textContent = months[month] + ' ' + year;
+		},
+
 		getDOMstrings: function() {
 			return DOMstrings;
 		}
@@ -353,6 +366,7 @@ var controller = (function(budgetCtrl, UICtrl) {
 
 	return {
 		init: function() {
+			UICtrl.displayMonth();
 			UICtrl.displayBudget({
 				budget: 0,
 				totalInc: 0,
